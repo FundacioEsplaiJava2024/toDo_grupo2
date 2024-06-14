7719b00a2466aa803745e25b273bd1229b61e77c
Rekloyd <paumorillasrivera129@gmail.com>
2024-06-14 09:27:39 +0200

    Fixed CSS

--- src/components/ProjectForm.tsx
+++ src/components/ProjectForm.tsx
@@ -1,27 +1,28 @@
-import React, {FormEvent, useState} from 'react'
+import React, { FormEvent, useState } from 'react'
 import { ProjectFormProps } from '../types'
 
 
 export const ProjectForm: React.FC<ProjectFormProps> = ({ addProject }) => {
   //React.FC especifica el tipado de los atributos que recibe la función 
-    const [value,setValue] = useState("")
+  const [value, setValue] = useState("")
 
-    const handleSubmit = (e : FormEvent) => {
-        e.preventDefault();
-        if(value.length < 1 || value.length >16){
-          alert("Enter a name between 1 and 16 characters");
-          return;
-        }
-        addProject(value);
-        setValue("");
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (value.length < 1 || value.length > 16) {
+      alert("Enter a name between 1 and 16 characters");
+      return;
     }
+    addProject(value);
+    setValue("");
+  }
 
-    return (
-      <form action="" className="project_form" onSubmit={handleSubmit}>
-          <input type="text" className='project_input' placeholder="Create project"  
-          onChange= {(e) => setValue(e.target.value)} value={value} />
-          <button  type="submit" className="project_button">New Project</button>
-          
-      </form>
-    )
+  return (
+    <form action="" className="project_form" onSubmit={handleSubmit}>
+      <div className="form_styles">
+        <input type="text" className='project_input' placeholder="Create project"
+          onChange={(e) => setValue(e.target.value)} value={value} />
+        <button type="submit" className="project_button">New Project</button>
+      </div>
+    </form>
+  )
 }
