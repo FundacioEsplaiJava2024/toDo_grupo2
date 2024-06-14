1e2b97a026562e5fd64a80bf71c03f03cf41fea4
Jaranag <joel.aranag@gmail.com>
2024-06-14 20:46:50 +0200

    Added functions that track and set the selected project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import './App.css'
 import { Sidebar } from './components/Sidebar'
+import { Project } from './types';
+import { useState } from 'react';
 
 function App() {
+
+  const [selectedProject, setSelectedProject] = useState<Project | null>(null);
+
+  const handleProjectSelect = (project: Project) => {
+    setSelectedProject(project);
+  };
 
   return (
     <>
