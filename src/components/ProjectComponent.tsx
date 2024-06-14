b46f4a72b55997672f450d749686412edbeb0dc4
Jaranag <joel.aranag@gmail.com>
2024-06-14 10:43:11 +0200

    added project component with fontAwesome icons that shows project name.

---
+++ src/components/ProjectComponent.tsx
@@ -0,0 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { ProjectComponentProps } from '../types'
+import React from 'react'
+
+export const ProjectComponent: React.FC<ProjectComponentProps> = ({project,deleteproject,editproject}) => {
+  return (
+    <div className='Project'>
+      <p>{project.projectName}</p>
+      <div className='icon_wrapper'>
+        <FontAwesomeIcon icon={faPenToSquare}  onClick={() => editproject(project.id)} className='faIcon' id="penIcon" />
+        <FontAwesomeIcon icon={faTrash} onClick={() => deleteproject(project.id)} className='faIcon' id="trashIcon"/>
+      </div>
+    </div>
+  )
+}
