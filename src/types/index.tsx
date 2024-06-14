43e00d29e04906bc7adedfed6da97b72e199dc7c
Jaranag <joel.aranag@gmail.com>
2024-06-14 10:43:21 +0200

    added project component typing

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -8,3 +8,10 @@ export interface Project {
 export interface ProjectFormProps {
   addProject: (projectName: string) => void;
 }
+
+export interface ProjectComponentProps {
+  key: string;
+  project: Project;
+  deleteproject: (id: string) => void;
+  editproject: (id: string) => void;
+}
