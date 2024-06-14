ee346c06ccb0792f04a91741b3a77a0c23160747
Jaranag <joel.aranag@gmail.com>
2024-06-14 10:35:19 +0200

    added projectForm and typing in types

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,6 +1,10 @@
 export interface Project {
-    id: string;
-    projectName: string;
-    completed: boolean;
-    isEditing: boolean;
-  }
+  id: string;
+  projectName: string;
+  completed: boolean;
+  isEditing: boolean;
+}
+
+export interface ProjectFormProps {
+  addProject: (projectName: string) => void;
+}
