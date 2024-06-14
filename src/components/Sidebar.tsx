77c67d39136ffd0c36833203b4cc2ac6498ee80f
Jaranag <joel.aranag@gmail.com>
2024-06-14 10:54:54 +0200

    added edit project function and input only activates when isEditing is active

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,9 @@
+import { ProjectForm } from './ProjectForm'
 import { useState } from 'react'
 import { Project } from '../types/index.tsx'
-import { ProjectForm } from './ProjectForm'
-import { ProjectComponent } from './ProjectComponent.tsx';
 import { v4 as uuidv4 } from "uuid";
-
+import { ProjectComponent } from './ProjectComponent.tsx';
+import { EditProjectForm } from './EditProjectForm.tsx';
 uuidv4();
 
 
@@ -21,29 +21,46 @@ export const Sidebar = () => {
     setProjects([...projects, newProject]);
   };
 
-  
   const deleteproject = (id: string) => {
 
     setProjects(projects.filter(project => project.id !== id))
 
   }
 
+  const editproject = (id: string) => {
+    setProjects(
+      projects.map((project) =>
+        project.id === id ? { ...project, isEditing: !project.isEditing } : project
+      )
+    );
+  };
 
+  const editProjectName = (projectName: string, id: string) => {
+    setProjects(
+      projects.map((p) =>
+        p.id === id? {...p, projectName, isEditing:!p.isEditing } : p
+      )
+    );
+  };
 
   return (
     <>
-    <section className="Sidebar">
-      <div className="sidebar_text">
-      <h1>Logo</h1>
-      <h3>Proyectos</h3>
-      </div>
-      <ProjectForm addProject={addProject} />
-      <div className='projectWrapper'>
-      {projects.map((project) => (
-          <ProjectComponent key={project.id} project={project} editproject={editproject} deleteproject={deleteproject} />
-      ))}
-      </div>
-    </section>
-  </>
+      <section className="Sidebar">
+        <div className="sidebar_text">
+        <h1>Logo</h1>
+        <h3>Proyectos</h3>
+        </div>
+        <ProjectForm addProject={addProject} />
+        <div className='projectWrapper'>
+        {projects.map((project) => (
+          project.isEditing ? (
+            <EditProjectForm key={project.id} editProject={editProjectName} project={project} />
+          ) : (
+            <ProjectComponent key={project.id} project={project} editproject={editproject} deleteproject={deleteproject} />
+          )
+        ))}
+        </div>
+      </section>
+    </>
   )
 }
