a24fad7ed4aec51d5f0b02fcf94c187be75e91d2
Jaranag <joel.aranag@gmail.com>
2024-06-14 10:34:37 +0200

    addProject and useState for projects added in sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useState } from 'react'
+import { Project } from '../types/index.tsx'
+import { ProjectForm } from './ProjectForm'
+import { v4 as uuidv4 } from "uuid";
+
+uuidv4();
+
+
+export const Sidebar = () => {
+
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  const addProject = (projectName: string) => {
+    const newProject: Project = {
+      id: uuidv4(),
+      projectName,
+      completed: false,
+      isEditing: false
+    };
+    setProjects([...projects, newProject]);
+  };
+
+
+
+  return (
+    <>
+      <section className="Sidebar">
+        <div className="sidebar_text">
+        <h1>Logo</h1>
+        <h3>Proyectos</h3>
+        </div>
+        <ProjectForm addProject={addProject} />
+      </section>
+    </>
+  )
+}
