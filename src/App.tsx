77616ddb81fcff8b2cd84525c172bfd4e3096a20
Rekloyd <paumorillasrivera129@gmail.com>
2024-06-15 13:36:37 +0200

    Created ToDoColumn component and html returned

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Sidebar } from "./components/Sidebar";
+import ToDoColumn from "./components/ToDoColumn";
 import { ToDoWrapper } from "./components/ToDoWrapper";
 import { Project } from "./types";
 import { useState } from "react";
 
 function App() {
+
   const [selectedProject, setSelectedProject] = useState<Project | null>(null);
 
   const handleProjectSelect = (project: Project) => {
@@ -13,10 +15,12 @@ function App() {
 
   return (
     <>
+    <div className="app_container">
       <Sidebar onProjectSelect={handleProjectSelect} />
-      {selectedProject && <ToDoWrapper project={selectedProject} />} 
+      <ToDoColumn />
+      {selectedProject && <ToDoWrapper project={selectedProject} />}
+      </div>
     </>
   );
 }
-
 export default App;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,6 +4,7 @@ import { Project } from '../types/index.tsx'
 import { v4 as uuidv4 } from "uuid";
 import { ProjectComponent } from './ProjectComponent.tsx';
 import { EditProjectForm } from './EditProjectForm.tsx';
+import ToDoColumn from './ToDoColumn.tsx';
 
 
 export const Sidebar: React.FC<SidebarProps> = ({ onProjectSelect }) => {
@@ -44,22 +45,23 @@ export const Sidebar: React.FC<SidebarProps> = ({ onProjectSelect }) => {
 
   return (
     <>
-      <section className="Sidebar">
-        <div className="sidebar_text">
-        <h1>Logo</h1>
-        <h3>Proyectos</h3>
-        </div>
-        <ProjectForm addProject={addProject} />
-        <div className='projectWrapper'>
-        {projects.map((project) => (
-          project.isEditing ? (
-            <EditProjectForm key={project.id} editProject={editProjectName} project={project} />
-          ) : (
-            <ProjectComponent key={project.id} project={project} editproject={startEditingProject} deleteproject={deleteproject} onClick={() => onProjectSelect(project)}/>
-          )
-        ))}
-        </div>
-      </section>
+        <section className="Sidebar">
+          <div className="sidebar_text">
+          <h1>Logo</h1>
+          <h3>Proyectos</h3>
+          </div>
+          <ProjectForm addProject={addProject} />
+          <div className='projectWrapper'>
+          {projects.map((project) => (
+            project.isEditing ? (
+              <EditProjectForm key={project.id} editProject={editProjectName} project={project} />
+            ) : (
+              <ProjectComponent key={project.id} project={project} editproject={startEditingProject} deleteproject={deleteproject} onClick={() => onProjectSelect(project)}/>
+            )
+          ))}
+          </div>
+        </section>
+
     </>
   )
 }
