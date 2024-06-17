80fbee13920ebb9ac44f169d2f25de3626cab3f1
Rekloyd <paumorillasrivera129@gmail.com>
2024-06-17 21:41:45 +0200

    Fixed column layout styles

--- src/components/ToDoWrapper.tsx
+++ src/components/ToDoWrapper.tsx
@@ -6,9 +6,11 @@ export const ToDoWrapper: React.FC<ToDoWrapperProps> = ({ project, addTask }) =>
     <div>
       <h1>Proyecto: </h1>
       <h2>{project.projectName}</h2>
+      <div className="column_container">
       <ToDoColumn key='1' project={project} addTask={addTask} title='TO DO 🎯' status='toDoTasks' />
       <ToDoColumn key='2' project={project} addTask={addTask} title='DOING 📓' status='doingTasks' />
       <ToDoColumn key='3' project={project} addTask={addTask} title='DONE ✔️' status='doneTasks' />
+      </div>
     </div>
   );
 };
