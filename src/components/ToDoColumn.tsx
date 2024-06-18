31010c046d910ff23b66006928180a6129ed096e
Rekloyd <paumorillasrivera129@gmail.com>
2024-06-18 19:03:51 +0200

    Deleted status icon on current column

--- src/components/ToDoColumn.tsx
+++ src/components/ToDoColumn.tsx
@@ -22,29 +22,34 @@ const ToDoColumn: React.FC<ToDoColumnProps> = ({
 
 
   return (
-      <>
-        <div className="to_do_column_container">
-          <h2 className="to_do_title">{title}</h2>
-          <div className="column_style">
-            {tasks.map((task) => (
-              <div className="task">
-                <p key={task.id} className="projectName">{task.taskName}</p>
-                <div className='icon_wrapper'>
-                  <FontAwesomeIcon icon={faBullseye} onClick={() => changeTaskStatus(task,"toDoTasks", status, project.id)} className='faIcon' id="penIcon" />
-                  <FontAwesomeIcon icon={faExclamation} onClick={() => changeTaskStatus(task,"doingTasks", status, project.id)} className='faIcon' id="penIcon" />
-                  <FontAwesomeIcon icon={faCheck} onClick={() => changeTaskStatus(task,"doneTasks", status, project.id)} className='faIcon' id="penIcon" />
-
-                  <FontAwesomeIcon icon={faTrash} onClick={() => deleteTask(task.id, project.id, status)} className='faIcon' id="trashIcon" />
-
-                </div>
-              </div>
-            ))}
-            <ToDoForm id={project.id} addTask={addTask} taskStatus={status} />
-          </div>
+    <>
+      <div className="to_do_column_container">
+        <h2 className="to_do_title">{title}</h2>
+        <div className="column_style">
+          {tasks.map((task) => (
+            <div className="task">
+              <p key={task.id} className="projectName">{task.taskName}</p>
+              <div className='icon_wrapper'>
+                      {status !== "toDoTasks" && ( 
+                        // es como un if(status != "toDoTasks"){} Si el estado es distinto a esta columna, lo carga, si coincide no lo muestra
+                        <FontAwesomeIcon icon={faBullseye} onClick={() => changeTaskStatus(task, "toDoTasks", status, project.id)} className='faIcon' id="penIcon" />
+                      )}
+                      {status !== "doingTasks" && (
+                        <FontAwesomeIcon icon={faExclamation} onClick={() => changeTaskStatus(task, "doingTasks", status, project.id)} className='faIcon' id="penIcon" />
+                      )}
+                      {status !== "doneTasks" && (
+                        <FontAwesomeIcon icon={faCheck} onClick={() => changeTaskStatus(task, "doneTasks", status, project.id)} className='faIcon' id="penIcon" />
+                      )}
+                      <FontAwesomeIcon icon={faTrash} onClick={() => deleteTask(task.id, project.id, status)} className='faIcon' id="trashIcon" />
+                    </div>
+            </div>
+          ))}
+          <ToDoForm id={project.id} addTask={addTask} taskStatus={status} />
         </div>
-      </>
-    );
-  };
+      </div>
+    </>
+  );
+};
 
 export default ToDoColumn;
 
