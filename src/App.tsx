7cdac8b1821528c2c6fa3ff86ca339e9ad36adfc
Jaranag <joel.aranag@gmail.com>
2024-06-21 13:52:48 +0200

    maybe testing but not working but it was worrking but now not working

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,31 @@ import { v4 as uuidv4 } from "uuid";
 import { TodoistApi } from "@doist/todoist-api-typescript";
 
 function App() {
-  const jiaToken: string = "41cff42f379ec3440dc13b7db401d8edd179a62b077a5a54125ab2e1599dd609";
+  const jiaToken: string =
+    "c0525743039cfaa82265235d7043ac1432b71b68";
   const api = new TodoistApi(jiaToken);
-  const [selectedProjectId, setSelectedProjectId] = useState<string | null>(null);
+  const [selectedProjectId, setSelectedProjectId] = useState<string | null>(
+    null
+  );
   const [projects, setProjects] = useState<Project[]>([]);
+  
+
+  const getProjects = async () => {
+    const apiProjects = await api.getProjects();
+    const newProjects: Project[] = Object.values(apiProjects).map((project) => {
+      return {
+        id: project.id,
+        projectName: project.name,
+        isEditing: false,
+        toDoTasks: [] as ToDoTask[],
+        doingTasks: [] as ToDoTask[],
+        doneTasks: [] as ToDoTask[],
+      };
+    });
+  
+    setProjects(newProjects); 
+    return newProjects;
+  };
 
   const addProject = (projectName: string) => {
     const newProject: Project = {
@@ -22,16 +43,15 @@ function App() {
       doneTasks: [],
     };
 
-    api.getProjects()
-      .then((projects) => console.log(projects))
-      .catch((error) => console.log(error));
-  
+    // api.getProjects()
+    //   .then((projects) => console.log(projects))
+    //   .catch((error) => console.log(error));
 
     return [...projects, newProject];
   };
 
   const deleteProject = (id: string) => {
-    const updatedProjects = projects.filter(project => project.id !== id);
+    const updatedProjects = projects.filter((project) => project.id !== id);
     if (selectedProjectId === id) {
       setSelectedProjectId(null);
     }
@@ -47,7 +67,9 @@ function App() {
 
   const editProjectName = (projectName: string, id: string) => {
     const updatedProjects = projects.map((project) =>
-      project.id === id ? { ...project, projectName, isEditing: false } : project
+      project.id === id
+        ? { ...project, projectName, isEditing: false }
+        : project
     );
     return updatedProjects;
   };
@@ -73,14 +95,20 @@ function App() {
     return updatedProjects;
   };
 
-  const deleteTask = (taskId: string, projectId: string, taskStatus: string) => {
+  const deleteTask = (
+    taskId: string,
+    projectId: string,
+    taskStatus: string
+  ) => {
     const updatedProjects = projects.map((project) => {
       if (project.id === projectId) {
         const tasksArray = project[taskStatus as keyof Project];
         if (Array.isArray(tasksArray)) {
           return {
             ...project,
-            [taskStatus]: tasksArray.filter((task: ToDoTask) => task.id !== taskId),
+            [taskStatus]: tasksArray.filter(
+              (task: ToDoTask) => task.id !== taskId
+            ),
           };
         }
       }
@@ -89,7 +117,12 @@ function App() {
     return updatedProjects;
   };
 
-  const changeTaskStatus = (task: ToDoTask, newStatus: string, oldStatus: string, projectId: string) => {
+  const changeTaskStatus = (
+    task: ToDoTask,
+    newStatus: string,
+    oldStatus: string,
+    projectId: string
+  ) => {
     const updatedProjects = projects.map((project) => {
       if (project.id === projectId) {
         const oldTasksArray = project[oldStatus as keyof Project];
@@ -97,7 +130,9 @@ function App() {
         if (Array.isArray(oldTasksArray) && Array.isArray(newTasksArray)) {
           return {
             ...project,
-            [oldStatus]: oldTasksArray.filter((t: ToDoTask) => t.id !== task.id),
+            [oldStatus]: oldTasksArray.filter(
+              (t: ToDoTask) => t.id !== task.id
+            ),
             [newStatus]: [...newTasksArray, task],
           };
         }
@@ -111,22 +146,31 @@ function App() {
     setSelectedProjectId(project.id);
   };
 
-  const selectedProject = projects.find((project) => project.id === selectedProjectId);
+  const selectedProject = projects.find(
+    (project) => project.id === selectedProjectId
+  );
 
   return (
     <div className="app_container">
+      <button onClick={getProjects()} >Boton de geteienar projects</button>
       <Sidebar
         projects={projects}
         addProject={(projectName) => setProjects(addProject(projectName))}
         deleteProject={(id) => setProjects(deleteProject(id))}
         startEditingProject={(id) => setProjects(startEditingProject(id))}
-        editProjectName={(projectName, id) => setProjects(editProjectName(projectName, id))}
+        editProjectName={(projectName, id) =>
+          setProjects(editProjectName(projectName, id))
+        }
         onProjectSelect={handleProjectSelect}
       />
       {selectedProject && (
         <ToDoWrapper
-          addTask={(taskName, taskStatus, projectId) => setProjects(addTask(taskName, taskStatus, projectId))}
-          deleteTask={(taskId, projectId, taskStatus) => setProjects(deleteTask(taskId, projectId, taskStatus))}
+          addTask={(taskName, taskStatus, projectId) =>
+            setProjects(addTask(taskName, taskStatus, projectId))
+          }
+          deleteTask={(taskId, projectId, taskStatus) =>
+            setProjects(deleteTask(taskId, projectId, taskStatus))
+          }
           changeTaskStatus={(task, newStatus, oldStatus, projectId) =>
             setProjects(changeTaskStatus(task, newStatus, oldStatus, projectId))
           }
