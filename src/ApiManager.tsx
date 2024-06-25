5d5f1d0d5e2193c8b69a5dbb01f9be9a6908ccbc
Jaranag <joel.aranag@gmail.com>
2024-06-25 23:59:10 +0200

    Now getting all tasks form api and inserting into pertinente project array

--- src/ApiManager.tsx
+++ src/ApiManager.tsx
@@ -1,36 +1,83 @@
 import { TodoistApi } from "@doist/todoist-api-typescript";
-import { Project, ToDoTask } from "./types";
+import { Project, ToDoTask, ApiTask } from "./types";
 
-
-const api: TodoistApi = new TodoistApi('d05d7c0c8bd5c1324acff07211be5beecb98610a');
+const api: TodoistApi = new TodoistApi(
+  "d05d7c0c8bd5c1324acff07211be5beecb98610a"
+);
 
 export function deleteApiProject(id: string) {
   api.deleteProject(id);
 }
 
+export const getApiProjects = async (): Promise<Project[]> => {
+  const apiProjects = await api.getProjects();
+  const newProjects: Project[] = await Promise.all(Object.values(apiProjects).map(async (project) => {
+    const projectTasks: [ApiTask[], ApiTask[], ApiTask[]] = await getProjectTasks(project.id);
+    return {
+      id: project.id,
+      projectName: project.name,
+      isEditing: false,
+      toDoTasks: projectTasks[0],
+      doingTasks: projectTasks[1],
+      doneTasks: projectTasks[2],
+    };
+  }));
+  return newProjects;
+};
 
-export const getApiProjects = async () => {
-    const apiProjects = await api.getProjects();
-    const newProjects: Project[] = Object.values(apiProjects).map((project) => {
-      return {
-        id: project.id,
-        projectName: project.name,
-        isEditing: false,
-        toDoTasks: [] as ToDoTask[],
-        doingTasks: [] as ToDoTask[],
-        doneTasks: [] as ToDoTask[],
-      };
+export const addApiTask = (
+  taskName: string,
+  projectId: string,
+  description: string
+) => {
+  api.addTask({
+    content: taskName,
+    projectId: projectId,
+    description: description,
+  });
+};
 
-    });
-    return newProjects;
+export const getAPITasks = async (projectId: string) => {
+  const allTasks = await api.getTasks({ projectId: projectId });
+  const convertedTasks: ApiTask[] = Object.values(allTasks).map((task) => {
+    return {
+      id: task.id,
+      content: task.content,
+      description: task.description
+    };
+  });
+  return convertedTasks;
+};
+
+export const getProjectTasks = async (projectId: string): Promise<[ToDoTask[], ToDoTask[], ToDoTask[]]> => {
+  const indexedTasks: [ToDoTask[], ToDoTask[], ToDoTask[]] = [[], [], []];
+  const convertedTasks = await getAPITasks(projectId);
+
+  convertedTasks.forEach(task => {
+    const convertedTask = convertTask(task);
+    switch (task.description) {
+      case "toDoTasks":
+        indexedTasks[0].push(convertedTask);
+        break;
+      case "doingTasks":
+        indexedTasks[1].push(convertedTask);
+        break;
+      case "doneTasks":
+        indexedTasks[2].push(convertedTask);
+        break;
+      default:
+        break;
+    }
+  });
+
+  return indexedTasks;
+};
+
+const convertTask = (apiTask: ApiTask) => {
+  const newTask: ToDoTask = {
+    id: apiTask.id,
+    taskName: apiTask.content,
+    isEditing: false,
   };
-
-
-export const addApiTask = (taskName:string, projectId:string, description:string) =>{
-  api.addTask({ content:taskName, projectId: projectId,description: description})
-}
-
-export const getApiTasks = (projectId:string)=>{
-
-  api.getTasks({ projectId: projectId })
+  return newTask
 }
